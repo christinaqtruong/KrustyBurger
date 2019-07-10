fa45c4ca3ebaa045b84003d1e68261687af4465c
Christina Truong <christinaqtruong@gmail.com>
2019-07-09 22:56:52 -0500

    Created sql files

---
+++ seeds.sql
@@ -0,0 +1,38 @@
+INSERT INTO menu (selection) 
+VALUES ('Mother Nature Burger');
+INSERT INTO menu (selection) 
+VALUES ('Krusty Burger');
+INSERT INTO menu (selection) 
+VALUES ('Double Krusty Burger'); 
+INSERT INTO menu (selection)
+VALUES ('Double Double Double Double');
+INSERT INTO menu (selection)
+VALUES ('Meat-Flavored Sandwich');
+INSERT INTO menu (selection)
+VALUES ('The Clogger');
+INSERT INTO menu (selection)
+VALUES ('Ribwich');
+INSERT INTO menu (selection)
+VALUES ('Filly Fudge Steak');
+INSERT INTO menu (selection)
+VALUES ('Bacon Balls');
+INSERT INTO menu (selection)
+VALUES ('Whatchamachicken');
+INSERT INTO menu (selection)
+VALUES ('Fish Sandwich');
+INSERT INTO menu (selection)
+VALUES ('Krusty-Partially-Gelatinated-Non-Dairy-Gum-Based-Beverage');
+INSERT INTO menu (selection)
+VALUES ('Krusty Popcorn');
+INSERT INTO menu (selection) 
+VALUES ('Frosted Krusty Flakes');
+INSERT INTO menu (selection) 
+VALUES ('Whattchamacarcass Sandwich');
+INSERT INTO menu (selection)
+VALUES ('Breakfast Balls');
+INSERT INTO menu (selection)
+VALUES ('Gravy Scrapems');
+INSERT INTO menu (selection)
+VALUES ('Twisty Lard');
+INSERT INTO menu (selection)
+VALUES ('Super-Choker Burritos with Macho Sauce');
